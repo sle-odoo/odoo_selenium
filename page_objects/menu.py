52b7dcaf435efb16993d828fdc4aad160ad5611f
Simon Lejeune <sle@openerp.com>
2016-11-13 00:41:31 +0100

    [REF] page objects: menu: improve selection
    The first implementation was a little too naive. Use the root property,
    check the top menu item only against the to menu items. Also, try a new
    API with `click` and `click_dropdown` to handle "primary" top menu items
    or the ones that open a dropdown.

--- page_objects/menu.py
+++ page_objects/menu.py
@@ -1,17 +1,53 @@
 # -*- coding: utf-8 -*-
 
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import ActionChains
+
 from page_objects.common import PageObject
 from page_objects.common import wait_rpc_done
 
 
 class Menu(PageObject):
 
-    def mouse_on_top_menu(self, menu_text):
-        self.driver.find_element_by_link_text(menu_text).move_to_element()
+    @property
+    def root(self):
+        return self.driver.find_element_by_class_name("o_main_navbar")
 
-    def click_on_top_menu(self, menu_text):
-        self.driver.find_element_by_link_text(menu_text).click()
+    def _find_top_menu(self, top_menu_text):
+        top_menu = None
+
+        top_menu_items = self.root\
+            .find_element_by_class_name("o_menu_sections")\
+            .find_elements_by_xpath('*')
+        for top_menu_item in top_menu_items:
+            if top_menu_item.text == top_menu_text:
+                top_menu = top_menu_item
+                break
+
+        if not top_menu:
+            raise NoSuchElementException(msg='Top menu "%s" was not found' % top_menu_text)
+
+        return top_menu
 
     @wait_rpc_done()
-    def click_on_menu(self, menu_text):
-        self.driver.find_element_by_link_text(menu_text).click()
+    def click_dropdown(self, top_menu_text, menu_text):
+        top_menu = self._find_top_menu(top_menu_text)
+        ActionChains(self.driver).move_to_element(top_menu).click(top_menu).perform()
+
+        # now that submenus are displayed, we can interact with them
+        menu = None
+        menu_items = top_menu\
+            .find_element_by_class_name("dropdown-menu")\
+            .find_elements_by_xpath("*")
+        for menu_item in menu_items:
+            if menu_item.text == menu_text:
+                menu = menu_item
+                break
+
+        if not menu:
+            raise NoSuchElementException(msg='Menu "%s" was not found' % top_menu_text)
+
+        menu.click()
+
+    def click(self, top_menu_text):
+        self._find_top_menu(top_menu_text).click()
