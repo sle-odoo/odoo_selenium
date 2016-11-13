1f424a76e32ca700b13a595a382b267a2bdb61df
Simon Lejeune <sle@openerp.com>
2016-11-13 15:20:48 +0100

    [REF] login: if already on login page, do not reload it
    When you log as different user duirng the same test, this speeds up
    things.

--- page_objects/login.py
+++ page_objects/login.py
@@ -15,25 +15,17 @@ class Login(object):
 
     def login(self, login, password):
         wait = WebDriverWait(self.driver, 10)
-        self.driver.get(self.instance_url)
-        wait.until(
-            expected_conditions.presence_of_element_located((By.NAME, "login"))
-        )
-
-        assert self.driver.title == "Odoo"
-        assert self.driver.current_url.endswith("/web/login")
+        if not self.driver.title == "Odoo" and not self.driver.current_url.endswith("/web/login"):
+            self.driver.get(self.instance_url)
+        wait.until(expected_conditions.presence_of_element_located((By.NAME, "login")))
 
         self.driver.find_element_by_name("login").send_keys(login)
         self.driver.find_element_by_name("password").send_keys(password)
         self.driver.find_element_by_name("password").submit()
 
-        wait.until(
-            expected_conditions.presence_of_element_located((By.CLASS_NAME, "o_web_client"))
-        )
+        wait.until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "o_web_client")))
 
-        wait.until(
-            expected_conditions.invisibility_of_element_located((By.CLASS_NAME, "o_loading"))
-        )
+        wait.until(expected_conditions.invisibility_of_element_located((By.CLASS_NAME, "o_loading")))
 
         self.driver.implicitly_wait(5)  # assets loading?
 
