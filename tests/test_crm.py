cb126b01e0f8321eed2dd99c1bc5f4945cb61fd8
Simon Lejeune <sle@openerp.com>
2016-11-21 09:15:18 +0100

    [FIX] tests: crm: adapt to previous refactoring

--- tests/test_crm.py
+++ tests/test_crm.py
@@ -12,9 +12,10 @@ class TestCRM(SeleniumCase):
 
         # Create two partners
         self.web_client.open_app_switcher()
+        self.assertTrue(self.web_client.app_switcher.is_opened())
         self.web_client.app_switcher.click_on_menu("CRM")
         self.assertTrue(self.web_client.kanban_view.is_opened())
-        self.web_client.menu.click_dropdown("Sales", "Customers")
+        self.web_client.menu.dropdown_click("Sales", "Customers")
         self.assertTrue(self.web_client.kanban_view.is_opened())
         self.web_client.kanban_view.create()
         self.assertTrue(self.web_client.form_view.is_opened())
@@ -27,29 +28,25 @@ class TestCRM(SeleniumCase):
         self.web_client.form_view.save()
 
         # Merge these two partners
-        self.web_client.control_panel.breadcrumbs.go_to_previous()
+        self.web_client.control_panel.breadcrumbs.previous_path_click()
         self.web_client.control_panel.view_switcher.to_list()
         self.web_client.control_panel.search_view.type_and_enter("Customer test")
-        self.web_client.list_view.select_all_rows()
-        self.web_client.control_panel.sidebar.click_on_action("Merge Selected Contacts")
+        self.web_client.list_view.row_select_all()
+        self.assertTrue(self.web_client.control_panel.sidebar.is_opened())
+        self.web_client.control_panel.sidebar.click("Merge Selected Contacts")
         self.assertTrue(self.web_client.wizard.is_opened())
         self.web_client.wizard.click_on_footer_buttons("MERGE CONTACTS")
         self.assertTrue(self.web_client.wizard.is_opened())
         self.web_client.wizard.click_on_footer_buttons("Close")
 
         # Check that the merge did something
-        self.web_client.list_view.click_on_row(1)
+        self.web_client.list_view.row_click(1)
         self.web_client.form_view.is_opened()
         self.assertEqual(
-            self.web_client.control_panel.breadcrumbs.get_current_step_value(),
+            self.web_client.control_panel.breadcrumbs.get_current_path(),
             "Customer test 1"
         )
         self.assertEqual(
             self.web_client.form_view.get_field_value_by_label_name("Job Position"),
             "Job position of customer test 2"
         )
-
-
-if __name__ == '__main__':
-    import unittest
-    unittest.main()
